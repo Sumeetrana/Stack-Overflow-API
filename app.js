ef47a3ac9e2925bba8e57115697cfc4cc2523b25
Sumeetrana <sumeetsinh28@gmail.com>
2020-05-05 12:28:02 +0530

    Returning questions with status code 200

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const axios = require('axios');
 const app = express();
 
 app.get('/:tagname', (req, res) => {
-    
-    console.log(req.params.tagname)        
+    axios.get(`https://api.stackexchange.com/2.2/questions?pagesize=10&order=desc&sort=creation&tagged=${req.params.tagname}&site=stackoverflow`)
+        .then((questions) => {
+            return res.status(200).json({questions: questions.data})
+        }) 
 })
 
 
